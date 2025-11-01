6bb9e82ddaf1260c60b1326b9265dd1333b67cf7
khawlaalaoui3 <khawla.alaoui@emsi-edu.ma>
2025-11-01 22:12:57 +0100

    Ajout de la configuration REST et ajout des dépendances dans pom.xml

--- src/main/java/ma/gov/pfe/tp3guidetouristiquealaouikhawla/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package ma.gov.pfe.tp3guidetouristiquealaouikhawla;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/ma/gov/pfe/tp3guidetouristiquealaouikhawla/config/RestConfiguration.java
@@ -0,0 +1,8 @@
+package ma.gov.pfe.tp3guidetouristiquealaouikhawla.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestConfiguration extends Application {
+}
